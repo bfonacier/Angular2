d3cd90189ec341ecf67e82bec576eecbf5d74aef
Bennett <bennett@fonacier.com>
2016-05-15 04:49:04 -0700

    my first component that compiled but did not run.

---
+++ app/my-component.component.ts
@@ -0,0 +1,10 @@
+import {Component}  from 'angular2/core';
+import {MyComponent}  from './my-component.component';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular2 App</h1><my-component><my-component>',
+  directives: [MyComponent]
+})
+
+export class AppCompnent {}
